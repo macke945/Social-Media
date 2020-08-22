df5e90f9dda7bc69c923fba034045b6d06e5c4db
macke945 <55479431+macke945@users.noreply.github.com>
2020-08-22 20:26:56 +0200

    add

---
+++ Data/DataTables/Dislike.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Social_Media.Data.DataTables
+{
+    public class Dislike
+    {
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public string PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
